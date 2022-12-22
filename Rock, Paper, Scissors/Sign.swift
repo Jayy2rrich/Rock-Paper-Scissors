57d582a0e1e152a28eb93c60173cf8f510965b26
Daniel <danielbonneau04@gmail.com>
2022-12-22 09:23:16 -0500

    12/21

---
+++ Rock, Paper, Scissors/Sign.swift
@@ -0,0 +1,61 @@
+//
+//  Sign.swift
+//  Rock, Paper, Scissors
+//
+//  Created by 7g on 12/20/22.
+//
+
+import Foundation
+
+enum Sign{
+    case rock
+    case paper
+    case scissors
+
+    var gesture: String{
+        if self == .rock{
+            return "👊"
+        }
+        else if self == .paper{
+            return "🖐"
+        }
+        else
+       {
+            return "✌️"
+        }
+    }
+    func randomSign() -> Sign {
+        let sign = Int.random(in: 0...2)
+        if sign == 0 {
+            return .rock
+        }
+        else if sign == 1 {
+            return .paper
+            
+        }
+        else {
+            return .scissors
+            
+        }
+    }
+    func checkWin(opponent: Sign) -> GameState {
+        switch (self, opponent) {
+        case (.rock, .scissors), (.paper, .rock), (.scissors, .paper):
+          return .win
+        case (.rock, .paper), (.paper, .scissors), (.scissors, .rock):
+          return .lose
+        default:
+          return .draw
+        }
+      }
+}
+    
+
+enum GameState {
+    case start
+    case win
+    case lose
+    case draw
+}
+
+
--- Rock, Paper, Scissors/ViewController.swift
+++ Rock, Paper, Scissors/ViewController.swift
@@ -8,12 +8,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var Status: UILabel!
+    @IBOutlet weak var AppSign: UILabel!
+    @IBOutlet weak var Paper: UIButton!
+    @IBOutlet weak var Rock: UIButton!
+    @IBOutlet weak var PlayAgain: UIButton!
+    @IBOutlet weak var Scissor: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
-}
-
+    @IBAction func PlayRock(_ sender: Any) {
+    }
+    
+    @IBAction func PlayPaper(_ sender: Any) {
+    }
+    @IBAction func PlayScissors(_ sender: Any) {
+    }
+    
+    @IBAction func PlayAgainButton(_ sender: Any) {
+    }
+    func updateUI(state: GameState) {
+      switch state {
+      case .win:
+        Status.text = "You win!"
+        view.backgroundColor = #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)
+      case .lose:
+        Status.text = "You lose!"
+        view.backgroundColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
+      case .draw:
+        Status.text = "It's a draw!"
+        view.backgroundColor = #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)
+      case .start:
+          Status.text = "Rock, paper, or scissors?"
+          view.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+          PlayAgain.isHidden = true
+          Rock.isEnabled = true
+          Paper.isEnabled = true
+          Scissor.isEnabled = true
+      }
+    }
